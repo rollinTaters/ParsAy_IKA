b20af5406fb41a97fe620305dc313f94e0117898
guvenchemy <23420210031@gazi.edu.tr>
2025-02-18 16:38:01 +0300

    Update the gauge, check it afterwards.

--- src/gauge.cpp
+++ src/gauge.cpp
@@ -1,25 +1,25 @@
 /*
-	MIT License
+    MIT License
 
-	Copyright (c) 2025 rollinTaters
+    Copyright (c) 2025 rollinTaters
 
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-	
-	The above copyright notice and this permission notice shall be included in all
-	copies or substantial portions of the Software.
-	
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 */
 
 
@@ -27,101 +27,116 @@
 
 ///// Gauge /////
 
-Gauge::Gauge( gauge_type gt )
+Gauge::Gauge(gauge_type gt)
 {
-    m_size = sf::Vector2f( 200, 200 );
-    m_pos = sf::Vector2f( 300, 50 );
+    m_size = sf::Vector2f(200, 200);
+    m_pos = sf::Vector2f(300, 50);
     m_dia = 200;
-    
+
     // TODO write a big switch statement for setting up different gauge types
-    switch( gt )
+    switch (gt)
     {
-        default:
+    default:
         m_max_value = 120;
         m_min_value = -10;
-        m_value     = 0;
-        
+        m_value = 0;
+
         m_needle_min_degree = -150;   // angle at which needle rests when min
         m_needle_max_degree = 150;
-        
-        m_red_start_value   = m_max_value * 0.80f;
-        m_red_end_value     = m_max_value;
-        
+
+        m_red_start_value = m_max_value * 0.80f;
+        m_red_end_value = m_max_value;
+
         m_green_start_value = m_max_value * 0.25f;
-        m_green_end_value   = m_max_value * 0.35f;
+        m_green_end_value = m_max_value * 0.35f;
         break;
-        case type1:
+    case type1:
         // stuff goes here
         break;
     }
     // setting gauge frame.
-    m_gaugeFrame.setRadius(m_dia/2);
+    m_gaugeFrame.setRadius(m_dia / 2);
     m_gaugeFrame.setFillColor(sf::Color::White);
     m_gaugeFrame.setOutlineThickness(5);
     m_gaugeFrame.setOutlineColor(sf::Color::Black);
-    m_gaugeFrame.setOrigin(m_dia/2,m_dia/2);
+    m_gaugeFrame.setOrigin(m_dia / 2, m_dia / 2);
     m_gaugeFrame.setPosition(m_pos.x + m_dia / 2, m_pos.y + m_dia / 2);
-    
+
     // uploading Font   
-    if (!m_font.loadFromFile("../assets/fonts/arial.ttf")){
+    if (!m_font.loadFromFile("..\\assets\\fonts\\arial.ttf")) {
         std::cerr << "Font yuklenemedi!";
     }
     // creating needle 
-    m_needle.setSize(sf::Vector2f(m_dia*0.65f,3)); // needle size
+    m_needle.setSize(sf::Vector2f(m_dia /2, 3)); // needle size
     m_needle.setFillColor(sf::Color::Red);
-    m_needle.setOrigin(0,1.5f); // making left end the rotation point
-    m_needle.setPosition(m_pos.x + m_dia / 2 , m_pos.y + m_dia / 2 ); // center of gauge
+    m_needle.setOrigin(0 , 1.5f); // making left end the rotation point
+    m_needle.setPosition(m_pos.x + m_dia / 2, m_pos.y + m_dia / 2); // center of gauge
     m_needle.setRotation(m_needle_min_degree); // starting point.
+	//std::cout << "Needle Position: " << m_needle.getPosition().x << ", " << m_needle.getPosition().y << std::endl; // for debugging
+    // center dot
+	m_center.setRadius(5);
+	m_center.setFillColor(sf::Color::Black);
+	m_center.setOrigin(5, 5);
+	m_center.setPosition(m_pos.x + m_dia / 2, m_pos.y + m_dia / 2);
 
-    // creating gauge marks
-
-    for (int i = 0 ; i<= 20; i++) {
-        float angle = (i * 9 + m_needle_min_degree ) * PI /180.0f ;
-        float outerRadius  = m_dia / 2;  
+    for (int i = 0; i <= 20; i++) {
+        // creating gauge marks
+        float angle = (i * 15 + m_needle_min_degree) * PI / 180.0f;
+        float outerRadius = m_dia / 2;
         float innerRadius = (i % 2 == 0) ? outerRadius - 20 : outerRadius - 10;
 
-        sf::Vector2f outerPoint(m_pos.x + outerRadius * cos(angle), m_pos.y + outerRadius * sin(angle));
-        sf::Vector2f innerPoint(m_pos.x + innerRadius * cos(angle), m_pos.y + innerRadius * sin(angle));
-        
+        sf::Vector2f center(m_pos.x + m_dia / 2, m_pos.y + m_dia / 2);
+        sf::Vector2f outerPoint(center.x + outerRadius * cos(angle),
+            center.y + outerRadius * sin(angle));
+        sf::Vector2f innerPoint(center.x + innerRadius * cos(angle),
+            center.y + innerRadius * sin(angle));
+
         m_ticks.push_back(sf::Vertex(outerPoint, sf::Color::Black));
         m_ticks.push_back(sf::Vertex(innerPoint, sf::Color::Black));
-
+        
+        // creating numbers
+        float textRadius = (m_dia / 2) - 30; 
+        
         if (i % 2 == 0) {
-            sf::Text text(std::to_string(i),m_font,18);
+            sf::Text text(std::to_string(i), m_font, 16);
             text.setFillColor(i <= 6 ? sf::Color::Black : (i <= 10 ? sf::Color::Yellow : sf::Color::Red));
-            float textRadius =  (m_dia/2) - 35; 
-
-            sf::Vector2f textPos ( m_pos.x + textRadius * cos(angle) -10,
-                                   m_pos.y + textRadius * sin(angle) -10 );
+            // getting text bounds to center text
+            sf::FloatRect textBounds = text.getLocalBounds();
+            text.setOrigin(textBounds.width / 2, textBounds.height / 2);
+			// get new calculated position
+            sf::Vector2f textPos(center.x + textRadius * cos(angle),
+                                 center.y + textRadius * sin(angle));
             text.setPosition(textPos);
-            m_numbers.push_back(text);                       
-
+            m_numbers.push_back(text);
         }
-    }
-    
-    // creating numbers
+    } 
 }
 
-void Gauge::updateVal( const float val )
+void Gauge::updateVal(const float val)
 {
-    m_value = val;
+        m_value = val;
 }
 
-void Gauge::updateProportionalVal( const float prop )
+
+void Gauge::updateProportionalVal(const float prop)
 {
-    m_value = (prop * (m_max_value-m_min_value)) + m_min_value;
+    m_value = (prop * (m_max_value - m_min_value)) + m_min_value;
 }
 
 void Gauge::render(sf::RenderTarget& target)
 {
     target.draw(m_gaugeFrame);
-    target.draw(m_ticks.data(),m_ticks.size(),sf::Lines);
-    for(const auto& num : m_numbers){
+    target.draw(m_ticks.data(), m_ticks.size(), sf::Lines);
+    for (const auto& num : m_numbers) {
         target.draw(num);
     }
-    float needleAngle = m_needle_min_degree + ((m_value - m_min_value) / (m_max_value - m_min_value)) * (m_needle_max_degree - m_needle_min_degree);
+	// needle rotation
+	float needleAngle = m_needle_min_degree + (m_value - m_min_value) * (m_needle_max_degree - m_needle_min_degree) / (m_max_value - m_min_value);
     m_needle.setRotation(needleAngle);
     target.draw(m_needle);
+	target.draw(m_center);
+
+	//std::cout << "m_value: " << m_value << " -> Needle Angle: " << needleAngle << std::endl; // for debugging
     // TODO render to global render_target
     // by adding sf::RenderTarget% target parameter i tried to provide some flexibility
 }
--- src/gauge.hpp
+++ src/gauge.hpp
@@ -1,25 +1,25 @@
 /*
-	MIT License
+    MIT License
 
-	Copyright (c) 2025 rollinTaters
+    Copyright (c) 2025 rollinTaters
 
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-	
-	The above copyright notice and this permission notice shall be included in all
-	copies or substantial portions of the Software.
-	
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 */
 
 
@@ -29,30 +29,31 @@
 #include <iostream>
 #define PI 3.141592
 
-enum gauge_type{
+enum gauge_type {
     type1,
     type2
 };
 
 class Gauge
 {
-  public:
-    Gauge( gauge_type );
+public:
+    Gauge(gauge_type);
 
-    void updateVal( const float );  // input is value to be displayed
-    void updateProportionalVal( const float );  // input is from 0.f to 1.f
+    void updateVal(const float);  // input is value to be displayed
+    void updateProportionalVal(const float);  // input is from 0.f to 1.f
     void render(sf::RenderTarget& target);
 
-  private:
+private:
     sf::CircleShape m_gaugeFrame;    // gauge shape
     std::vector<sf::Vertex> m_ticks; // lines
     std::vector<sf::Text> m_numbers; // numbers in gauge
+	sf::CircleShape m_center;        // center dot of gauge
     sf::Font m_font;          // font type
     sf::Vector2f m_size;    // in pixels
     sf::Vector2f m_pos;     // of top left corner
     sf::RectangleShape m_needle; // needle 
     float m_dia;        // diameter in pixels
-    
+
     float m_max_value;
     float m_min_value;
     float m_value;
