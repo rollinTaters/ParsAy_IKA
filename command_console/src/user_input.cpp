b6d4f6580a0e4bfcbf4203ba90069ad1d6319a2b
Güven Yılmaz <23420210031@gazi.edu.tr>
2025-03-18 14:48:57 +0300

    added userr input

--- command_console/src/user_input.cpp
+++ command_console/src/user_input.cpp
@@ -1,1 +1,31 @@
 #include "user_input.hpp"
+
+UserInput::UserInput()
+{
+
+}
+
+void UserInput::switchDriveMode(DriveMode newMode)
+{
+    m_drive_mode = newMode;
+}
+
+void UserInput::proccesInput()
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    {
+        std::cout << "Accelerate n go forward" << std::endl;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    {
+        std::cout << "Go backward" << std::endl;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
+        std::cout << "Turn right" << std::endl;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
+        std::cout << "Turn left" << std::endl;
+    }
+}
