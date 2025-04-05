ac47f82befbcc1f6fa8793e92534b13e65552219
Taters <rollin.taters@gmail.com>
2025-04-05 05:25:12 +0300

    Quaternion class is renamed to allow raylibs quaternion struct

--- common_code/src/utility.cpp
+++ common_code/src/utility.cpp
@@ -163,6 +163,14 @@ Point& Point::operator*=( const float & f )
     return *this;
 }
 
+Point& Point::operator/=( const float & f )
+{
+    x/=f;
+    y/=f;
+    z/=f;
+    return *this;
+}
+
 Point operator+( const Point &p1, const Point &p2 )
 {
     return Point( p1.x+p2.x, p1.y+p2.y, p1.z+p2.z );
@@ -180,7 +188,7 @@ Point operator*( const Point &p1, const float f )
 
 // ---- End of Point ----
 
-Quaternion &Quaternion::operator=(const Quaternion &q)
+cQuaternion &cQuaternion::operator=(const cQuaternion &q)
 {
 	w = q.w;
 	x = q.x;
@@ -189,7 +197,7 @@ Quaternion &Quaternion::operator=(const Quaternion &q)
 	return *this;
 }
 
-Quaternion &Quaternion::operator*=(const Quaternion &q)
+cQuaternion &cQuaternion::operator*=(const cQuaternion &q)
 {
 	float w1 = w;
 	float x1 = x;
@@ -208,17 +216,17 @@ Quaternion &Quaternion::operator*=(const Quaternion &q)
 	return *this;
 }
 
-float Quaternion::dot(const Quaternion &q) const
+float cQuaternion::dot(const cQuaternion &q) const
 {
 	return w * q.w + x * q.x + y * q.y + z * q.z;
 }
 
-float Quaternion::norm() const
+float cQuaternion::norm() const
 {
 	return sqrtf(w * w + x * x + y * y + z * z);
 }
 
-Quaternion &Quaternion::normalize()
+cQuaternion &cQuaternion::normalize()
 {
 	float iLen = 1 / norm();
 	w *= iLen;
@@ -228,12 +236,12 @@ Quaternion &Quaternion::normalize()
 	return *this;
 }
 
-const Quaternion Quaternion::conjugate() const
+const cQuaternion cQuaternion::conjugate() const
 {
-	return Quaternion(w, -x, -y, -z);
+	return cQuaternion(w, -x, -y, -z);
 }
 
-void Quaternion::rotateVector( sf::Vector3f &vec ) const
+void cQuaternion::rotateVector( v3f &vec ) const
 {
 	// t = 2q x v
 	float tx = 2. * (y * vec.z - z * vec.y);
@@ -246,7 +254,7 @@ void Quaternion::rotateVector( sf::Vector3f &vec ) const
 	vec.z = vec.z + w * tz + x * ty - y * tx;
 }
 
-const Quaternion Quaternion::fromEuler( sf::Vector3f euler ) 
+const cQuaternion cQuaternion::fromEuler( v3f euler ) 
 {
 	float x = euler.x * 0.5;
 	float y = euler.y * 0.5;
@@ -261,16 +269,16 @@ const Quaternion Quaternion::fromEuler( sf::Vector3f euler )
 	float sZ = sinf(z);
 
     // YXZ
-	return Quaternion(
+	return cQuaternion(
 		sX * sY * sZ + cX * cY * cZ,
 		sX * sZ * cY + sY * cX * cZ,
 		sX * cY * cZ - sY * sZ * cX,
 		sZ * cX * cY - sX * sY * cZ);
 }
 
-const Quaternion Quaternion::fromAxisAngle( sf::Vector3f axis, float radian ) 
+const cQuaternion cQuaternion::fromAxisAngle( v3f axis, float radian ) 
 {
-	Quaternion ret;
+	cQuaternion ret;
 
 	float halfAngle = radian * 0.5;
 
@@ -290,22 +298,22 @@ const Quaternion Quaternion::fromAxisAngle( sf::Vector3f axis, float radian )
 
 BB3D::BB3D()
 {
-    m_pos = sf::Vector3f(0,0,0);
-    m_size = sf::Vector3f(1,1,1);
+    m_pos = v3f(0,0,0);
+    m_size = v3f(1,1,1);
 }
 
-BB3D::BB3D( const sf::Vector3f pos,
-            const Quaternion quat,
-            const sf::Vector3f size ):
+BB3D::BB3D( const v3f pos,
+            const cQuaternion quat,
+            const v3f size ):
     m_pos(pos),m_quat(quat),m_size(size)
 {
 }
 
 void BB3D::yawLeft( float radian )
 {
-    sf::Vector3f axis = getLocalVecZ();
+    v3f axis = getLocalVecZ();
     // create a quat that rotates us in local csys
-    Quaternion q = Quaternion::fromAxisAngle( axis, radian );
+    cQuaternion q = cQuaternion::fromAxisAngle( axis, radian );
 
     // apply quat to our member quat
     m_quat *= q;
@@ -313,9 +321,9 @@ void BB3D::yawLeft( float radian )
 
 void BB3D::pitchUp( float radian )
 {
-    sf::Vector3f axis = getLocalVecX();
+    v3f axis = getLocalVecX();
     // create a quat that rotates us in local csys
-    Quaternion q = Quaternion::fromAxisAngle( axis, radian );
+    cQuaternion q = cQuaternion::fromAxisAngle( axis, radian );
 
     // apply quat to our member quat
     m_quat *= q;
@@ -323,67 +331,67 @@ void BB3D::pitchUp( float radian )
 
 void BB3D::rollRight( float radian )
 {
-    sf::Vector3f axis = getLocalVecY();
+    v3f axis = getLocalVecY();
     // create a quat that rotates us in local csys
-    Quaternion q = Quaternion::fromAxisAngle( axis, radian );
+    cQuaternion q = cQuaternion::fromAxisAngle( axis, radian );
 
     // apply quat to our member quat
     m_quat *= q;
 }
 
-void BB3D::translateLocal( const sf::Vector3f t )
+void BB3D::translateLocal( const v3f t )
 {
     m_pos += getLocalVecX() * t.x +
              getLocalVecY() * t.y +
              getLocalVecZ() * t.z;
 }
-void BB3D::translateFWD( const float meter )   { translateLocal(sf::Vector3f(meter,0,0)); }
-void BB3D::translateRight( const float meter ) { translateLocal(sf::Vector3f(0,meter,0)); }
-void BB3D::translateUp( const float meter )    { translateLocal(sf::Vector3f(0,0,meter)); }
+void BB3D::translateFWD( const float meter )   { translateLocal(v3f(meter,0,0)); }
+void BB3D::translateRight( const float meter ) { translateLocal(v3f(0,meter,0)); }
+void BB3D::translateUp( const float meter )    { translateLocal(v3f(0,0,meter)); }
 
 // -- Setters --
-void BB3D::setSize( const sf::Vector3f size )
+void BB3D::setSize( const v3f size )
 {
     m_size = size;
 }
 
 void BB3D::setSize( const float x, const float y, const float z )
 {
-    m_size = sf::Vector3f(x,y,z);
+    m_size = v3f(x,y,z);
 }
 
-void BB3D::setPos( const sf::Vector3f pos )
+void BB3D::setPos( const v3f pos )
 {
     m_pos = pos;
 }
 
 void BB3D::setPos( const float x, const float y, const float z )
 {
-    m_pos = sf::Vector3f(x,y,z);
+    m_pos = v3f(x,y,z);
 }
 
-void BB3D::setAng( const sf::Vector3f euler )
+void BB3D::setAng( const v3f euler )
 {
-    m_quat = Quaternion::fromEuler( euler );
+    m_quat = cQuaternion::fromEuler( euler );
 }
 
 void BB3D::setAng( const float x, const float y, const float z )
 {
-    setAng( sf::Vector3f(x,y,z) );
+    setAng( v3f(x,y,z) );
 }
 
 // -- Getters --
-sf::Vector3f BB3D::getSize() const { return m_size; }
+v3f BB3D::getSize() const { return m_size; }
 
-sf::Vector3f BB3D::getPos() const { return m_pos; }
+v3f BB3D::getPos() const { return m_pos; }
 
-sf::Vector3f BB3D::getAngEuler() const
+v3f BB3D::getAngEuler() const
 {
     // conversion yanked from, then modified:
     // https://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToEuler/indexLocal.htm
 
     // TODO axis naming convention must be checked
-    Quaternion q1 = m_quat;
+    cQuaternion q1 = m_quat;
 	double sqw = q1.w*q1.w;
     double sqx = q1.x*q1.x;
     double sqy = q1.y*q1.y;
@@ -392,28 +400,28 @@ sf::Vector3f BB3D::getAngEuler() const
     float bank = std::atan2(2.0 * (q1.y*q1.z + q1.x*q1.w),(-sqx - sqy + sqz + sqw));
     float attitude = std::asin(-2.0 * (q1.x*q1.z - q1.y*q1.w));
 
-    return sf::Vector3f( attitude, bank, heading );
+    return v3f( attitude, bank, heading );
 }
 
-Quaternion BB3D::getQuaternion() const { return m_quat; }
+cQuaternion BB3D::getQuaternion() const { return m_quat; }
 
-sf::Vector3f BB3D::getLocalVecX() const
+v3f BB3D::getLocalVecX() const
 {
-    sf::Vector3f ret(1,0,0);
+    v3f ret(1,0,0);
     m_quat.rotateVector( ret );
     return ret;
 }
 
-sf::Vector3f BB3D::getLocalVecY() const 
+v3f BB3D::getLocalVecY() const 
 {
-    sf::Vector3f ret(0,1,0);
+    v3f ret(0,1,0);
     m_quat.rotateVector( ret );
     return ret;
 }
 
-sf::Vector3f BB3D::getLocalVecZ() const
+v3f BB3D::getLocalVecZ() const
 {
-    sf::Vector3f ret(0,0,1);
+    v3f ret(0,0,1);
     m_quat.rotateVector( ret );
     return ret;
 }
--- common_code/src/utility.hpp
+++ common_code/src/utility.hpp
@@ -32,7 +32,9 @@
 
 #include <cmath>
 
+#ifndef PI
 #define PI 3.141592
+#endif
 
 
 struct Point
@@ -56,14 +58,17 @@ struct Point
     Point& operator-=( const Point & );
     Point& operator*=( const Point & );
     Point& operator*=( const float & );
+    Point& operator/=( const float & );
 
 };
+
+typedef Point v3f;
 
 Point operator+( const Point &, const Point & );
 Point operator-( const Point &, const Point & );
 Point operator*( const Point &, const float );
 
-class Quaternion
+class cQuaternion
 {
   public:
     float w;
@@ -71,25 +76,25 @@ class Quaternion
     float y;
     float z;
 
-    Quaternion() : w(1), x(0), y(0), z(0) {}
-    Quaternion( const Quaternion &q ) : w(q.w), x(q.x), y(q.y), z(q.z) {}
-    Quaternion( float _x, float _y, float _z ) : w(0), x(_x), y(_y), z(_z) {}
-    Quaternion( float _w, float _x, float _y, float _z ) : w(_w), x(_x), y(_y), z(_z) {}
+    cQuaternion() : w(1), x(0), y(0), z(0) {}
+    cQuaternion( const cQuaternion &q ) : w(q.w), x(q.x), y(q.y), z(q.z) {}
+    cQuaternion( float _x, float _y, float _z ) : w(0), x(_x), y(_y), z(_z) {}
+    cQuaternion( float _w, float _x, float _y, float _z ) : w(_w), x(_x), y(_y), z(_z) {}
 
-    Quaternion &operator=( const Quaternion &rhs );
-    Quaternion &operator*=( const Quaternion &q );
-    const Quaternion operator*( const Quaternion &q ) const { return Quaternion(*this) *= q; }
+    cQuaternion &operator=( const cQuaternion &rhs );
+    cQuaternion &operator*=( const cQuaternion &q );
+    const cQuaternion operator*( const cQuaternion &q ) const { return cQuaternion(*this) *= q; }
 
-    float dot( const Quaternion &q ) const;
+    float dot( const cQuaternion &q ) const;
     float norm() const;
-    Quaternion &normalize();
+    cQuaternion &normalize();
 
-    const Quaternion conjugate() const;
-    void rotateVector( sf::Vector3f &vec ) const;
+    const cQuaternion conjugate() const;
+    void rotateVector( v3f &vec ) const;
 
     // these create a new quaternion and return it
-    static const Quaternion fromEuler( sf::Vector3f euler );
-    static const Quaternion fromAxisAngle( sf::Vector3f axis, float radian );
+    static const cQuaternion fromEuler( v3f euler );
+    static const cQuaternion fromAxisAngle( v3f axis, float radian );
 };
 
 // 3 dimensional bounding box geometry
@@ -97,9 +102,9 @@ struct BB3D
 {
   public:
     BB3D();
-    BB3D( const sf::Vector3f pos,
-          const Quaternion quat,
-          const sf::Vector3f size );
+    BB3D( const v3f pos,
+          const cQuaternion quat,
+          const v3f size );
 
     // these rotate around the local object axes
     void yawLeft( float radian );
@@ -107,33 +112,33 @@ struct BB3D
     void rollRight( float radian );
 
     // these translate on the local object axes
-    void translateLocal( const sf::Vector3f );
+    void translateLocal( const v3f );
     void translateFWD( const float meter );
     void translateRight( const float meter );
     void translateUp( const float meter );
 
 
     // -- setters --
-    void setSize( const sf::Vector3f size );
+    void setSize( const v3f size );
     void setSize( const float x, const float y, const float z );
 
-    void setPos( const sf::Vector3f pos );
+    void setPos( const v3f pos );
     void setPos( const float x, const float y, const float z );
 
-    void setAng( const sf::Vector3f euler );    // (radian)
+    void setAng( const v3f euler );    // (radian)
     void setAng( const float x, const float y, const float z ); // (radian)
 
     // -- getters --
-    sf::Vector3f getSize() const;
+    v3f getSize() const;
 
-    sf::Vector3f getPos() const;
+    v3f getPos() const;
 
-    sf::Vector3f getAngEuler() const;   // euler angles (in order: z_yaw, x_pitch, y_roll) (radian)
-    Quaternion getQuaternion() const;   // returns quaternion describing the rotation of local csys
+    v3f getAngEuler() const;   // euler angles (in order: z_yaw, x_pitch, y_roll) (radian)
+    cQuaternion getQuaternion() const;   // returns quaternion describing the rotation of local csys
 
-    sf::Vector3f getLocalVecX() const;
-    sf::Vector3f getLocalVecY() const;
-    sf::Vector3f getLocalVecZ() const;
+    v3f getLocalVecX() const;
+    v3f getLocalVecY() const;
+    v3f getLocalVecZ() const;
 
   private:
     // unless otherwise specified, all 3d vectors are:
@@ -141,11 +146,11 @@ struct BB3D
     // +X:pitch up, +Y:roll right, +Z:yaw left      (radian)
 
     // positional variables
-    sf::Vector3f m_pos;      // (metre)
-    Quaternion m_quat;       // a quaternion describing our rotation relative to global
+    v3f m_pos;      // (metre)
+    cQuaternion m_quat;       // a quaternion describing our rotation relative to global
 
     // size variable
-    sf::Vector3f m_size;     // (metre)
+    v3f m_size;     // (metre)
 
     friend BB3D operator +( BB3D left, BB3D right );
     friend BB3D& operator +=( BB3D& left, BB3D right );
